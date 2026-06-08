92b67eef877019507bb507a9241874ece0f694c6
Gojo-67 <iplatanyi@gmail.com>
2026-06-08 17:53:45 +0100

    add main logic

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const container =document.querySelector("#animeContainer")
 const searchInput = document.querySelector("#searchInput")
-
-async function fetchAnime(query="naruto"){
-    const response = await fetch(`https://api.jikan.moe/v4/anime?q=${query}&limit=12`)
+const modal=document.querySelector("#modal")
+const closeModal=document.querySelector("#closeModal")
+let timeout=null
+async function fetchAnime(){
+    const response = await fetch(`https://api.jikan.moe/v4/anime?limit=12`)
     const data = await response.json()
     displayAnime(data.data)
 }
@@ -11,21 +13,60 @@ function displayAnime(animeList){
     container.innerHTML = ""
 
     animeList.forEach(anime => {
-        container.innerHTML+=`
-            <div class="card">
+        const card=document.createElement("div")
+        card.classList.add('card')
+        card.innerHTML=`
+          
                 <img src ="${anime.images.jpg.image_url}">
                 <div class="card-content">
                     <h2>${anime.title}</h2>
                     <p class="rating">${anime.score}</p>
-                    <p class="discription">${anime.synopsis}</p>
+                    
                 </div>
-            </div>
+           
         `
-        
+    card.addEventListener('click',()=>openModal(anime))    
+    container.appendChild(card)
     });
 }
 
 searchInput.addEventListener("input",(event)=>{
-    fetchAnime(event.target.value)
+    clearTimeout(timeout)
+    timeout=setTimeout(()=>{
+fetchAnime(event.target.value.trim())
+    },500)
+    
 })
+
+/* =========================
+   MODAL
+========================= */
+function openModal(anime) {
+  document.getElementById('modalImg').src =
+    anime.images.jpg.large_image_url;
+
+  document.getElementById('modalTitle').textContent =
+    anime.title;
+
+  document.getElementById('modalRating').textContent =
+    `⭐ Rating: ${anime.score || 'N/A'}`;
+
+  document.getElementById('modalGenres').textContent =
+    `Genres: ${anime.genres.map(g => g.name).join(', ')}`;
+
+  document.getElementById('modalSynopsis').textContent =
+    anime.synopsis || 'No description';
+
+  modal.classList.remove('hidden');
+}
+
+function close() {
+  modal.classList.add('hidden');
+}
+
+closeModal.addEventListener('click', close);
+
+modal.addEventListener('click', (e) => {
+  if (e.target === modal) close();
+});
 fetchAnime()
